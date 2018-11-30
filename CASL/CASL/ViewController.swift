47ed0c1a6d791424e7125df69fa125302f8c04b9
Garage <Garage@Garages-MacBook-Pro.local>
2018-11-30 13:25:33 -0500

    initial revision

---
+++ CASL/CASL/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  CASL
+//
+//  Created by Garage on 11/28/18.
+//  Copyright © 2018 Garage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //MARK: Actions
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func signupButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func forgotButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func backButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func backButton2(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
